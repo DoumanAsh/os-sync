b82803b957385783354ccb8a0c96e6ffd713fce5
Douman <douman@gmx.se>
2020-02-17 18:18:25 +0100

    0.2.0

--- tests/sem.rs
+++ tests/sem.rs
@@ -5,15 +5,16 @@ fn should_return_when_signaled() {
     let sem = Sem::new(0).unwrap();
 
     assert!(sem.try_lock().is_none());
-    assert!(sem.post());
     #[cfg(not(any(target_os = "macos", target_os = "ios")))]
     {
+        assert!(sem.post());
         assert!(!sem.post());
     }
 
     #[cfg(any(target_os = "macos", target_os = "ios"))]
     {
-        assert!(sem.post());
+        assert!(!sem.post());
+        assert!(!sem.post());
     }
 
     let _guard = sem.lock();
