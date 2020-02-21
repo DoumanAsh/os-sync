8437d9b28a0aefc3c5017e8caf9bd52c97d4594e
Douman <douman@gmx.se>
2020-02-21 21:44:40 +0100

    Once: graceful handling of semaphore creation fail

--- src/once.rs
+++ src/once.rs
@@ -6,6 +6,19 @@ use core::sync::atomic::{AtomicU8, AtomicU32, AtomicPtr, Ordering};
 const INCOMPLETE: u8 = 0x0;
 const RUNNING: u8 = 0x1;
 const COMPLETE: u8 = 0x2;
+const FAIL: u8 = 0x4;
+
+const FAIL_MSG: &str = "Unable to initialize semaphore";
+
+struct StateGuard<'a> {
+    state: &'a AtomicU8,
+}
+
+impl<'a> Drop for StateGuard<'a> {
+    fn drop(&mut self) {
+        self.state.store(COMPLETE, Ordering::Release);
+    }
+}
 
 struct SemGuard {
     sem: Sem,
@@ -63,23 +76,22 @@ impl Once {
         loop {
             match self.state.load(Ordering::Acquire) {
                 COMPLETE => break,
+                FAIL => panic!(FAIL_MSG),
                 INCOMPLETE => {
                     if INCOMPLETE != self.state.compare_and_swap(INCOMPLETE, RUNNING, Ordering::Acquire) {
                         continue;
                     }
 
-                    struct StateGuard<'a> {
-                        state: &'a AtomicU8,
-                    }
-
-                    impl<'a> Drop for StateGuard<'a> {
-                        fn drop(&mut self) {
-                            self.state.store(COMPLETE, Ordering::Release);
+                    let sem = match Sem::new(0) {
+                        Some(sem) => sem,
+                        None => {
+                            self.state.store(FAIL, Ordering::Acquire);
+                            panic!(FAIL_MSG);
                         }
-                    }
+                    };
 
                     let mut sem_guard = SemGuard {
-                        sem: Sem::new(0).unwrap(),
+                        sem,
                         waiting: AtomicU32::new(0),
                     };
                     self.sem.store(&mut sem_guard as *mut _, Ordering::Release);
@@ -97,6 +109,10 @@ impl Once {
 
                     let mut sem = self.sem.load(Ordering::Acquire);
                     while sem.is_null() {
+                        if self.state.load(Ordering::Acquire) == FAIL {
+                            panic!(FAIL_MSG);
+                        }
+
                         sem = self.sem.load(Ordering::Acquire);
                         core::sync::atomic::spin_loop_hint();
                     }
